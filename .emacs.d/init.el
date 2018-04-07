76dd30a4bec39b28b54d87498f4d93fbc8074a77
Junichiro Takagi <t.junichiro@gmail.com>
2018-04-07 22:48:33 +0900

    update confs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -155,16 +155,6 @@
 ;;______________________________________________________________________
 
 ;; go-mode
-(require 'go-mode)
-(add-hook 'go-mode-hook
-	  '(lambda ()
-	     (setq c-basic-offset 4)
-             (setq indent-tabs-mode t)
-             (local-set-key (kbd "M-.") 'godef-jump)
-             (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
-             (local-set-key (kbd "C-c i") 'go-goto-imports)
-             (local-set-key (kbd "C-c d") 'godoc)))
-(add-hook 'before-save-hook 'gofmt-before-save)
 (require 'go-autocomplete)
 (require 'auto-complete-config)
 (ac-config-default)
