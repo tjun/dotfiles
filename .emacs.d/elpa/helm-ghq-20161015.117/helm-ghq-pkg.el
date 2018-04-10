d1993b8332353cbf8c70efbecba1158066a2b784
Junichiro Takagi <t.junichiro@gmail.com>
2018-04-10 10:26:38 +0900

    add emacs lisps

---
+++ .emacs.d/elpa/helm-ghq-20161015.117/helm-ghq-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-ghq" "20161015.117" "ghq with helm interface" '((helm "2.2.0")) :commit "49481685adbb25b3f766aabfdb7472becc673a94" :url "https://github.com/masutaka/emacs-helm-ghq")
