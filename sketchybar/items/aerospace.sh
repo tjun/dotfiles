ed58546b22faeb2c324999bfb8c23bb12401c613
Junichiro Takagi <t.junichiro@gmail.com>
2026-01-14 10:21:44 +0900

    update

--- sketchybar/items/aerospace.sh
+++ sketchybar/items/aerospace.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
+sketchybar --add event aerospace_workspace_change
 
 sketchybar --add item spacer.1 left \
 	--set spacer.1 background.drawing=off \
@@ -8,16 +8,18 @@ sketchybar --add item spacer.1 left \
 	icon.drawing=off \
 	width=10
 
-for i in {0..9}; do
-	sid=$((i + 1))
-	sketchybar --add space space.$sid left \
-		--set space.$sid associated_space=$sid \
-		label.drawing=off \
-		icon.padding_left=10 \
-		icon.padding_right=10 \
-		background.padding_left=-5 \
-		background.padding_right=-5 \
-		script="$PLUGIN_DIR/space.sh"
+for sid in $(aerospace list-workspaces --all); do
+    sketchybar --add item space.$sid left \
+        --subscribe space.$sid aerospace_workspace_change \
+        --set space.$sid \
+        background.color=0x44ffffff \
+        background.corner_radius=5 \
+        background.height=20 \
+        background.drawing=off \
+        label="$sid" \
+				label.padding_right=10 \
+        click_script="aerospace workspace $sid" \
+        script="$PLUGIN_DIR/aerospace.sh $sid"
 done
 
 sketchybar --add item spacer.2 left \
